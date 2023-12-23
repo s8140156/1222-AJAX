ea4ebf667064b8320f5e7677ac44813d46444ab9
s8140156 <s8140156@gmail.com>
2023-12-24 01:43:28 +0800

    使用ajax來載入全部學生資料及理解by jq query(def)程式去拆解要送進api的程式後再回傳前台看後台執行後的結果

--- app2/api/query.php
+++ app2/api/query.php
@@ -9,6 +9,10 @@ switch($_GET['do']){
 		// 在php要帶資料出去前先把格式改成json, 就不用在js那邊轉格式
 		echo json_encode($Student->all());
 	break;
+	case "def":
+		dd($_GET);
+		//解析GET送過來的有do=def, 還有陣列拆分[0]=>sex, [1]=>1
+	break;
 }
 
 
