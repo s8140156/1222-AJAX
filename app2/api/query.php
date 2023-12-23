c292aebbb678b7055354529809e8ce99b9158494
s8140156 <s8140156@gmail.com>
2023-12-24 00:32:58 +0800

    開始jquery與php資料庫串接使用應用 理解queryAll jq程式寫法串接api邏輯

--- app2/api/query.php
+++ app2/api/query.php
@@ -1,12 +1,13 @@
 <?php
 //處理查詢資料的請求
 
-include_once "";
+include_once "db.php";
 
 switch($_GET['do']){
 	case "all":
+        header('Content-Type: application/json; charset=utf-8');
+		// 在php要帶資料出去前先把格式改成json, 就不用在js那邊轉格式
 		echo json_encode($Student->all());
-
 	break;
 }
 
